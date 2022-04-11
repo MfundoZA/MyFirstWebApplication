3a21b0e4c89a74dab4032b30b1bae11a48063957
Mfundo <jordantenza@gmai.com>
2022-04-11 16:47:51 +0200

    Refactored code to get data from a SQL rather than JSON file

--- MyFirstWebApplication/Controllers/HomeController.cs
+++ MyFirstWebApplication/Controllers/HomeController.cs
@@ -7,25 +7,53 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace MyFirstWebApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        public JsonFileCakeService CakeService;
-        public IEnumerable<Cake> Cakes { get; private set; }
+        // public JsonFileCakeService CakeService;
+        public static string ConnnectionString { get; private set; }
+        public static SqlConnection Connection { get; set; }
+        public List<Cake> Cakes { get; private set; }
 
         public HomeController(ILogger<HomeController> logger,
             JsonFileCakeService cakeService)
         {
             _logger = logger;
-            CakeService = cakeService;
+            // CakeService = cakeService;
+            ConnnectionString = "Data Source=JORDAN;Initial Catalog=\"Mbali'sBakery\";Integrated Security=True";
+            Connection = new SqlConnection(ConnnectionString);
         }
 
         public IActionResult Index()
         {
-            Cakes = CakeService.GetProducts();
+            Cakes = new List<Cake>();
+
+            
+
+            Connection.Open();
+            if (Connection.State == System.Data.ConnectionState.Open)
+            {
+                string getAllCakes = "SELECT * FROM Cake";
+                SqlCommand command = new SqlCommand(getAllCakes, Connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    Cake cake = new Cake();
+                    cake.cakeId = (int) reader[0];
+                    cake.name = (string) reader[1];
+                    cake.description = (string) reader[2];
+                    cake.price = Decimal.ToDouble((decimal)reader[3]);
+                    cake.path = (string) reader[4];
+
+                    Cakes.Add(cake);
+                }
+            }
             return View(this);
         }
 
